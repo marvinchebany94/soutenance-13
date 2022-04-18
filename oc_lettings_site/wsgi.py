f4ec192a892761b269dfe2561fe26e90e1586ee0
marvinchebany94 <marvinchebany94190@gmail.com>
2022-04-18 16:41:13 +0200

    .

--- oc_lettings_site/wsgi.py
+++ oc_lettings_site/wsgi.py
@@ -1,7 +1,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
 
 application = get_wsgi_application()
